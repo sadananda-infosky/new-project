cd1a44f31c05a16fcb91aabeca15e7f1ed224760
sadananda-infosky <sadananda@infosky.co.in>
2026-03-27 17:39:56 +0530

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{fontFamily:'Arial', padding:'40px'}}>
+      <h1>🚀 React App Ready</h1>
+      <p>Your landing page will be built here.</p>
+    </div>
+  )
+}
